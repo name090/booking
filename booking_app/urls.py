31d5ae4077b023c4a3e00b84837642af42d87eec
name090 <mukolanimets0@gmail.com>
2026-06-09 20:37:02 +0300

    Add booking confirmation feature with form handling and new templates

--- booking_app/urls.py
+++ booking_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home_page'),
     path('apartment/<int:apartment_id>/', views.booking, name='booking_page'),
+        path('confirmation/<int:booking_id>/', views.booking_confirmation, name='booking_confirmation')
 ]
--- booking_app/views.py
+++ booking_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Apartments, Category, Booking
 
 # Create your views here.
@@ -10,6 +10,32 @@ def home(request):
 def booking(request, apartment_id):
 
     apartments = Apartments.objects.get(id=apartment_id)
-    context = {"apart": apartments}
 
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        check_in = request.POST.get('check_in')
+        check_out = request.POST.get('check_out')
+
+        booking = Booking.objects.create(
+            name=name,
+            email=email,
+            phone=phone,
+            apartment=apartments,
+            check_in=check_in,
+            check_out=check_out,
+            user=request.user
+        )
+
+
+        return redirect('booking_confirmation', booking_id=booking.id)
+
+    context = {"apart": apartments}
     return render(request, 'booking.html', context)
+
+
+def booking_confirmation(request, booking_id):
+    booking = Booking.objects.get(id=booking_id)
+    apartment = booking.apartment
+    return render(request, 'booking_confirmation.html', {'booking': booking, 'apart': apartment})
